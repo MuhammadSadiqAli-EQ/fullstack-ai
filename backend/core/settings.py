2ef8eb6ffee453d67ddde58bcaa9b7de62ab0e54
Muhammad Sadiq Ali <ma10379@st.habib.edu.pk>
2026-08-04 03:37:12 +0500

    deployment fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     # "lb_health_check.middleware.AliveCheck",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -83,7 +84,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
